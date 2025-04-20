e952889475faf0e5866d197a52a52f9b76b4d1ab
auilk <abdelghafour.lbaz01@gmail.com>
2025-04-20 17:49:50 +0100

    Setup project

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App()
+{
+  return (
+    <div className="w-dvw h-dvh flex justify-center items-center">
+      <h1 className="text-9xl text-center font-bold text-white">CLAMPLI</h1>
+    </div>
+  );
+}
+
+export default App;
