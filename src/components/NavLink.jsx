b21c0aeeda4105165f52a4bd684448d9f817cd45
auilk <abdelghafour.lbaz01@gmail.com>
2025-04-25 05:29:44 +0100

    Implement NavLink component

---
+++ src/components/NavLink.jsx
@@ -0,0 +1,43 @@
+import { useContext } from "react";
+import { NavBarContext } from "./NavBar";
+
+/**
+ * @param {Object} props
+ * @param {"left" | "center" | "right"} [props.position = "center"]
+ * @param {string} props.link
+ * @param {string} [props.text = "click"]
+ * @param {string} [props.textColor = "white"]
+ * @param {number} [props.fontSize = 1] - Font size is in rem
+ * @param {string} props.icon - Icon path
+ * @param {number} props.iconScale - Icon path
+ * @param {React.ReactNode} props.children
+ * @returns {JSX.Element}
+ */
+
+function NavLink({position = "center", link, text = "click", textColor = "white", fontSize = 1, icon, iconScale = 1})
+{
+    const context = useContext(NavBarContext);
+
+    if (!context)
+    {
+        throw new Error("<NavLink> can only be used within <NavBar>.");
+    }
+
+    return (
+        <a href={link} target="_blank" rel="noopener noreferrer" className="flex flex-col justify-center items-center cursor-pointer">
+            <div className="rounded-full bg-contain bg-center bg-no-repeat"
+                style={{
+                    width: `${1.25 * iconScale}rem`,
+                    height: `${1.25 * iconScale}rem`,
+                    backgroundImage: `url(${icon})`,
+                }}></div>
+            <p className="font-bold"
+                style={{
+                    color: textColor,
+                    fontSize: `${fontSize}rem`
+                }}>{text}</p>
+        </a>
+    );
+}
+
+export default NavLink;
