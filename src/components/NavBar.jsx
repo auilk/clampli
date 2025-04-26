7bb703b9ecd9d52da384cd4ab6ce2ce2e83be44c
auilk <abdelghafour.lbaz01@gmail.com>
2025-04-26 22:17:33 +0100

    Implement NavBar component

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,66 @@
+import React, { createContext } from "react";
+
+/**
+ * @param {Object} props
+ * @param {number} [props.gap = 5] - Gap in pixels
+ */
+
+const NavBarContext = createContext(false);
+
+function NavBar({children, gap = 5})
+{
+    const leftElements = []
+    const centerElements = []
+    const rightElements = []
+
+    React.Children.forEach(children, (child) =>
+    {
+        if (!React.isValidElement(child)) return;
+
+        switch (child.props.position)
+        {
+            case "left":
+                leftElements.push(child)
+                break;
+            case "center":
+                centerElements.push(child);
+                break;
+            case "right":
+                rightElements.push(child);
+                break;
+            default:
+                centerElements.push(child);
+                break;
+        }
+    });
+
+    return(
+        <NavBarContext.Provider value={true}>
+            <nav className="m-2 flex border-t-2 border-b-2 border-white"
+                style={{
+                    gap: `${gap}px`,
+                }}>
+                <div className="pl-10 pr-10 pt-3 pb-3 ml-8 flex justify-center items-center border-l-2 border-r-2 border-white"
+                    style={{
+                        gap: `${gap}px`,
+                    }}>
+                    {leftElements}
+                </div>
+                <div className="flex justify-center items-center flex-grow" 
+                    style={{
+                        gap: `${gap}px`,
+                    }}>
+                    {centerElements}
+                </div>
+                <div className="pr-10 pl-10 pt-3 pb-3 mr-8 flex justify-center items-center border-r-2 border-l-2 border-white" 
+                    style={{
+                        gap: `${gap}px`,
+                    }}>
+                    {rightElements}
+                </div>
+            </nav>
+        </NavBarContext.Provider>
+    );
+}
+
+export { NavBar, NavBarContext };
