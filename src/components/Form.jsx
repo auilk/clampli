a5ca9e31eeb89b0e15674fa81162be63d722ea79
auilk <abdelghafour.lbaz01@gmail.com>
2025-05-15 15:57:10 +0100

    Implement core Form layout

---
+++ src/components/Form.jsx
@@ -0,0 +1,16 @@
+import InputField from "./InputField.jsx"
+import UnitSelector from "./UnitSelector.jsx";
+
+function Form()
+{
+    return(
+        <form className="w-fit h-fit" onSubmit={(event) => event.preventDefault()}>
+            <div className="pl-10 pr-10 pt-30 pb-30 flex flex-col gap-25 border-[2px] rounded-b-md border-white bg-graphite-900">
+                <InputField label="Min Viewport size" placeholder="Enter your value..."></InputField>
+                <InputField label="Max Viepoert size" placeholder="Enter your value..."></InputField>
+            </div>
+        </form>
+    );
+}
+
+export default Form;
