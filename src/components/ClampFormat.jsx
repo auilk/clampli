a97c430e3c61e301175856cdf3c83482ccb85db7
auilk <abdelghafour.lbaz01@gmail.com>
2025-05-17 13:15:05 +0100

    Finish ClampFormat component animations

--- src/components/ClampFormat.jsx
+++ src/components/ClampFormat.jsx
@@ -6,18 +6,22 @@ function ClampFormat()
 
     return (
         <button className="w-fit h-fit flex bg-white" onClick={() => SetFormat(format === "css" ? "tailwind" : "css")}>
-            <div className="w-fit h-fit text-5xl text-graphite-900 font-bold leading-normal relative">
-                <span className="pl-10 pr-10 items-center"
+            <div className="w-fit h-fit text-5xl text-graphite-900 font-bold leading-normal relative perspective-dramatic">
+                <span className="pl-10 pr-10 opacity-0">TAILWIND</span>
+                <span className="pl-10 pr-10 absolute left-1/2 top-1/2 -translate-1/2 origin-[center_center_-20px] backface-hidden transition-all duration-500"
                 style={{
-                    opacity: format === "tailwind" ? 1 : 0
+                    transform: format === "tailwind" ? "rotateX(180deg)" : "rotateX(0deg)"
                 }}>TAILWIND</span>
-                <span className="pl-10 pr-10 items-center absolute left-1/2 top-1/2 -translate-1/2"
+                <span className="pl-10 pr-10 absolute left-1/2 top-1/2 -translate-1/2 origin-[center_center_-20px] backface-hidden transition-all duration-500"
                 style={{
-                    opacity: format === "css" ? 1 : 0
+                    transform: format === "tailwind" ? "rotateX(0deg)" : "rotateX(-180deg)"
                 }}>CSS</span>
             </div>
-            <svg viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg" className="h-full">
-                    <path d={format === "css" ? "M20 43 L50 68 80 43" : "M20 57 L50 32 80 57"} stroke="#0E0E0E" strokeWidth="8" strokeLinecap="round" strokeLinejoin="round"/>
+            <svg viewBox="0 0 100 100" fill="#0E0E0E" xmlns="http://www.w3.org/2000/svg" className="h-full relative transition-all duration-500"
+            style={{
+                transform: format === "tailwind" ? "rotateX(0deg)" : "rotateX(-180deg)"
+            }}>
+                <path d="M20 60 L50 32 80 60 Z" stroke="#0E0E0E" strokeWidth="8" strokeLinecap="round" strokeLinejoin="round"/>
             </svg>
         </button>
     );
