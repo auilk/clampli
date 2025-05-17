2c76cab94c0671ee7dcdf8e448fc6c85aaffca18
auilk <abdelghafour.lbaz01@gmail.com>
2025-05-17 14:45:53 +0100

    Add customization props to ClampFormat component

--- src/components/ClampFormat.jsx
+++ src/components/ClampFormat.jsx
@@ -1,23 +1,27 @@
 import { useState } from "react";
 
-function ClampFormat()
+function ClampFormat({fontSize = 1, horizontalPadding = 2.5})
 {
     const [format, SetFormat] = useState("css");
 
     return (
         <button className="w-fit h-fit flex bg-white" onClick={() => SetFormat(format === "css" ? "tailwind" : "css")}>
-            <div className="w-fit h-fit text-5xl text-graphite-900 font-bold leading-normal relative perspective-dramatic">
-                <span className="pl-10 pr-10 opacity-0">TAILWIND</span>
-                <span className="pl-10 pr-10 absolute left-1/2 top-1/2 -translate-1/2 origin-[center_center_-20px] backface-hidden transition-all duration-500"
+            <div className="w-fit h-fit text-graphite-900 font-bold leading-normal relative perspective-dramatic"
+            style={{
+                padding: `0px ${horizontalPadding}rem`,
+                fontSize: `${fontSize}rem`
+            }}>
+                <span className="opacity-">TAILWIND</span>
+                <span className="absolute left-1/2 top-1/2 -translate-1/2 origin-[center_center_-20px] backface-hidden transition-transform duration-500"
                 style={{
                     transform: format === "tailwind" ? "rotateX(180deg)" : "rotateX(0deg)"
                 }}>TAILWIND</span>
-                <span className="pl-10 pr-10 absolute left-1/2 top-1/2 -translate-1/2 origin-[center_center_-20px] backface-hidden transition-all duration-500"
+                <span className="absolute left-1/2 top-1/2 -translate-1/2 origin-[center_center_-20px] backface-hidden transition-transform duration-500"
                 style={{
                     transform: format === "tailwind" ? "rotateX(0deg)" : "rotateX(-180deg)"
                 }}>CSS</span>
             </div>
-            <svg viewBox="0 0 100 100" fill="#0E0E0E" xmlns="http://www.w3.org/2000/svg" className="h-full relative transition-all duration-500"
+            <svg viewBox="0 0 100 100" fill="#0E0E0E" xmlns="http://www.w3.org/2000/svg" className="h-full relative transition-transform duration-500"
             style={{
                 transform: format === "tailwind" ? "rotateX(0deg)" : "rotateX(-180deg)"
             }}>
