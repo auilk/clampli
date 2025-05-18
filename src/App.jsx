a9549b131a3b861eaf5e230e095a9d6e9f872f9b
auilk <abdelghafour.lbaz01@gmail.com>
2025-05-18 19:44:20 +0100

    Complete main page by adding and styling all components

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,52 @@ import Form from "./components/Form";
 import ClampFormat from "./components/ClampFormat";
 import ClampResult from "./components/ClampResult";
 
+
 function App()
 {
   return (
     <div className="flex flex-col h-dvh">
-      <ClampResult></ClampResult>
+      <NavBar gap={10}>
+        <NavLink 
+          position="right" 
+          link="https://github.com/auilk" 
+          text="GitHub" 
+          fontSize={0.6} 
+          icon={GitIcon}
+        ></NavLink>
+
+        <p 
+          className="text-white text-lg" 
+          position="center"
+        >
+          Fluid type & layout—no math, just magic
+        </p>
+
+        <div 
+          className="w-15 mr-1.5 h-10 scale-160 bg-cover bg-center bg-no-repeat"
+          position="left"
+          style={{
+            backgroundImage: `url(${ClampliLogo})`,
+          }}
+        ></div>
+      </NavBar>
+
+      <div className="p-5 flex-1">
+        <DottedBG className="h-full flex justify-center">
+          <div className="w-full ml-80 mr-80 flex flex-col justify-center gap-10">
+            <div className="flex justify-between">
+              <Form></Form>
+
+              <Form></Form>
+            </div>
+
+            <div className="flex justify-center">
+              <ClampResult></ClampResult>
+            </div>
+          </div>
+        </DottedBG>
+      </div>
+
     </div>
   );
 }
