39e47e78fc6f48e3d2288c6bc4f7be9e7a914627
auilk <abdelghafour.lbaz01@gmail.com>
2025-05-20 16:29:35 +0100

    Minor design update and animation for InputField

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -11,29 +11,33 @@ import { useState } from "react";
  * @returns {JSX.Element}
  */
 
-function InputField({label = "label", placeholder = "placeholder", width = 20, textSize = 1, textColor, borderColor})
+function InputField({label = "label", width = 20, textSize = 1, textColor, borderColor})
 {
     const [value, SetValue] = useState("");
+    const [isFocused, SetIsFocused] = useState(false);
 
     return (
         <div className="flex flex-col relative">
             <label 
-                className="pl-2 pr-2 text-lg text-white absolute -top-3.5 left-2 bg-graphite-900" 
+                className="pl-2 pr-2 text-lg text-white bg-graphite-900 absolute top-1/2 left-1 -translate-y-1/2 transition-all"
                 htmlFor={label}
                 style={{
-                    color: borderColor
+                    color: borderColor,
+                    top: isFocused === true ? "-2px" : "50%"
                 }}
             >
                 {label}
             </label>
             
             <input 
-                className="pl-3 pr-3 pt-3 pb-3 border-2 rounded-md outline-0 text-6xl text-white" 
+                className="pl-3 pr-3 pt-3 pb-3 border-2 outline-0 text-6xl text-white" 
                 id={label} 
                 type="text" 
                 value={value} 
-                placeholder={placeholder} 
                 onChange={(event) => SetValue(event.target.value)}
+                onFocus={() => SetIsFocused(true)}
+                onBlur={() => SetIsFocused(false)}
+                on
                 style={{
                     width: `${width}rem`,
                     fontSize: `${textSize}rem`,
