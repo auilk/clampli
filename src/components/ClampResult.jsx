0edee038c4f60873675addcc8bf1d772aff7d177
auilk <abdelghafour.lbaz01@gmail.com>
2025-06-19 19:01:04 +0100

    Add ClipBoard component to copy text to clipboard

--- src/components/ClampResult.jsx
+++ src/components/ClampResult.jsx
@@ -1,6 +1,7 @@
 import useResultStore from "../store/result-store";
 import useSelectorStore from "../store/selector-store";
 import useToggleStore from "../store/toggle-store";
+import ClipBoard from "./ClipBoard";
 import Toggle from "./Toggle";
 import UnitSelector from "./UnitSelector";
 
@@ -19,9 +20,6 @@ function ClampResult({fontSize = "1rem", borderWidth = "2px"})
 
     const resultUnit = useSelectorStore((state) => (state.resultUnit));
     const setResultUnit = useSelectorStore((state) => (state.setResultUnit));
-
-    const minElement = useResultStore((state) => state.minElement);
-    const maxElement = useResultStore((state) => state.maxElement);
 
     const clampResult = useResultStore((state) => state.clampResult);
 
@@ -57,18 +55,7 @@ function ClampResult({fontSize = "1rem", borderWidth = "2px"})
                         {clampResult}
                     </code>
 
-                    <svg
-                        className="cursor-pointer"
-                        width="clamp(10px, 7.982px + 0.631vw, 24px)" 
-                        height="clamp(10px, 7.982px + 0.631vw, 24px)" 
-                        viewBox="0 -960 960 960" 
-                        fill="#e3e3e3"
-                        xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <path 
-                            d="M360-240q-33 0-56.5-23.5T280-320v-480q0-33 23.5-56.5T360-880h360q33 0 56.5 23.5T800-800v480q0 33-23.5 56.5T720-240H360Zm0-80h360v-480H360v480ZM200-80q-33 0-56.5-23.5T120-160v-560h80v560h440v80H200Zm160-240v-480 480Z"
-                        />
-                    </svg>
+                    <ClipBoard textToCopy={clampResult}></ClipBoard>
                 </div>
 
                 <Toggle
