b9f5393fdd0e68b90acfd600195beacf43a6c745
auilk <abdelghafour.lbaz01@gmail.com>
2025-06-19 14:58:11 +0100

    Install vite-plugin-gh-pages and deploy app to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import tailwindcss from "@tailwindcss/vite"
+import { ghPages } from "vite-plugin-gh-pages"
 
 export default defineConfig(
 {
-  plugins: [react(), tailwindcss()]
+  base: "/Clampli/",
+  plugins: [react(), tailwindcss(), ghPages()]
 })
