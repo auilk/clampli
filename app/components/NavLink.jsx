c2f3e57c1b82392be0daaf6f9b4dbc8abfd9afc1
auilk <abdelghafour.lbaz01@gmail.com>
2025-07-03 16:19:02 +0100

    Port Navbar and Navlink components from Vite to Next.js

---
+++ app/components/NavLink.jsx
@@ -0,0 +1,64 @@
+"use client";
+
+import { useContext } from "react";
+import { NavBarContext } from "./NavBar";
+import Image from "next/image";
+
+/**
+ * Renders a styled navigation link with optional icon and custom content.
+ *
+ * @param {Object} props - Component properties.
+ * @param {"left" | "center" | "right"} [props.position="center"] - Alignment of the link.
+ * @param {string} props.link - Destination URL.
+ * @param {string} [props.text="click"] - Display text for the link.
+ * @param {string} [props.textColor="white"] - CSS color value for the text.
+ * @param {string} [props.fontSize="1rem"] - Font size of the text (e.g., "1rem", "16px").
+ * @param {string} props.icon - URL or path to the icon.
+ * @param {string} [props.iconSize="1.25rem"] - CSS size of the icon (e.g., "1.25rem", "20px").
+ * @returns {JSX.Element} The rendered navigation link.
+ */
+
+function NavLink({position = "center", link, text = "click", textColor = "white", fontSize = "1rem", icon, iconSize = "1.25rem"})
+{
+    const context = useContext(NavBarContext);
+
+    if (!context)
+    {
+        throw new Error("<NavLink> can only be used within <NavBar>.");
+    }
+
+    return (
+        <a 
+            className="flex flex-col justify-center items-center cursor-pointer" 
+            href={link} 
+            target="_blank" 
+            rel="noopener noreferrer"
+        >
+            <div
+                className="relative"
+                style={{
+                    width: iconSize,
+                    height: iconSize,
+                }}
+            >
+                <Image
+                    src={icon}
+                    fill
+                    className="object-cover"
+                ></Image>
+            </div>
+
+            <p 
+                className="font-bold"
+                style={{
+                    color: textColor,
+                    fontSize: fontSize
+                }}
+            >
+                {text}
+            </p>
+        </a>
+    );
+}
+
+export default NavLink;
--- app/page.js
+++ app/page.js
@@ -1,7 +1,59 @@
+import Image from "next/image";
+import { NavBar } from "./components/NavBar";
+import NavLink from "./components/NavLink";
+
 export default function Home() {
   return (
-    <div className="w-dvw h-dvh flex justify-center items-center">
-      <h1 className="text-7xl text-white font-bold">Clampli</h1>
-    </div>
+    <header>
+      <header className="flex flex-col">
+        <NavBar 
+          gap="clamp(5px, 4.286px + 0.223vw, 10px)"
+          padding="clamp(0.1rem, 0.028rem + 0.36vw, 0.6rem)"
+          borderWidth="clamp(1px, 0.614px + 0.12vw, 3px)"
+        >
+          <NavLink 
+            position="right" 
+            link="https://github.com/auilk" 
+            text="GitHub" 
+            fontSize="clamp(0.35rem, 0.283rem + 0.337vw, 0.7rem)"
+            icon="/icons/github-icon.png"
+            iconSize="clamp(0.75rem, 0.625rem + 0.627vw, 1.4rem)"
+          ></NavLink>
+
+          <NavLink 
+            position="right" 
+            link="https://github.com/auilk" 
+            text="LinkedIn" 
+            fontSize="clamp(0.35rem, 0.283rem + 0.337vw, 0.7rem)"
+            icon="/icons/linkedin-icon.png"
+            iconSize="clamp(0.75rem, 0.625rem + 0.627vw, 1.4rem)"
+          ></NavLink>
+
+          <p 
+            position="center"
+            className="text-white"
+            style={{
+              fontSize: "clamp(0.6rem, 0.465rem + 0.675vw, 1.3rem)"
+            }}
+          >
+            Fluid type & layout—no math, just magic
+          </p>
+
+          <div 
+            className="mx-1.5 h-10 relative"
+            position="left"
+            style={{
+              width: "clamp(2.5rem, 1.825rem + 3.373vw, 6rem)",
+            }}
+          >
+            <Image
+              src="/logos/clampli-logo.png"
+              fill
+              className="object-cover"
+            ></Image>
+          </div>
+        </NavBar>
+      </header>
+    </header>
   );
 }
