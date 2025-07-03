7b84ffc9232f3085a2b5237a9376c6efb7968c0d
auilk <abdelghafour.lbaz01@gmail.com>
2025-07-03 15:53:44 +0100

    Create nextjs branch and initialize blank Next.js project for migration

---
+++ app/page.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="w-dvw h-dvh flex justify-center items-center">
+      <h1 className="text-7xl text-white font-bold">Clampli</h1>
+    </div>
+  );
+}
